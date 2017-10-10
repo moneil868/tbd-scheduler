006b43c563345308bbc0406d74702725a97b777b
sbcreates <drawsarahb@gmail.com>
2017-10-10 16:59:54 -0400

    Added json response to cobalt api. Added sessions methods for user log in and log out

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,16 @@
 class CoursesController < ApplicationController
+require "HTTParty"
+
+  def index
+
+  end
+
+
+  private
+
+  def course_filter_search(query)
+    response = HTTParty.get("http://localhost:4242/1.0/courses/filter?q=name:'#{query}' OR code:'#{query}'")
+
+    JSON.parse(response.body)
+  end
 end
