422db91720fbdc9a594ed8bd3363f6350fb6b4d8
sbcreates <drawsarahb@gmail.com>
2017-10-10 17:08:46 -0400

    Created routes for controllers, created log in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
       redirect_to root_url, notice: "Logged in!"
     else
       render :new
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login' => 'sessions#new', :as => :login
+
+  delete 'logout' => 'sessions#destroy', :as => :logout
+
+  resources :courses
+  resource :users
+  resource :sessions, only: [:create]
+
+
 end
