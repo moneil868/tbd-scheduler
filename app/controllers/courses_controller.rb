bc0727ee10dada0548b863be91b53e9f5df4c20a
sbcreates <drawsarahb@gmail.com>
2017-10-10 19:50:24 -0400

    Deleted old code that isn't needed in course controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,7 @@
 class CoursesController < ApplicationController
 
   def index
-    # @courses = Course.search(params[:course])
-
-    # @courses = if params[:course]
-    #   Course.where('course_code LIKE ?', "%#{params[:course]}%")
-    # else
-    #   Course.all
-    # end
     @courses = []
-
     if params[:course]
       response = HTTParty.get("http://localhost:4242/1.0/courses/filter?q=name:'#{params[:course]} OR code:'#{params[:course]}&limit=100")
       parsed_reponse = JSON.parse(response.body)
@@ -21,15 +13,6 @@ class CoursesController < ApplicationController
         }
       end
     end
-
   end
 
-
-  private
-
-  def course_filter_search(query)
-    response = HTTParty.get("http://localhost:4242/1.0/courses/filter?q=name:'#{query}' OR code:'#{query}'")
-
-    JSON.parse(response.body)
-  end
 end
