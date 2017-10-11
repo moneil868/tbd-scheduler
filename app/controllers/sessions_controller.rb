94610d36bb592f23e37700719c238e847cd41171
sbcreates <drawsarahb@gmail.com>
2017-10-10 20:37:15 -0400

    Added log out button. Checked that a user and sign up, log in, and log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
-      session[:user_id] = user_id
+      session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
       render :new
