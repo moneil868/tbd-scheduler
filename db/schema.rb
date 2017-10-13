d432f6353520f6fdadd106a6d7a246e69615f005
sbcreates <drawsarahb@gmail.com>
2017-10-13 15:47:44 -0400

    Migrated instructor data to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013192806) do
+ActiveRecord::Schema.define(version: 20171013193227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20171013192806) do
     t.integer "level"
     t.string "campus"
     t.string "term"
+  end
+
+  create_table "instructors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "meeting_sections", force: :cascade do |t|
