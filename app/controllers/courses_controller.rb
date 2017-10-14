203e3ee085b4d6bf39374f755b67059a95bbe428
Marlon O'Neil <hello@marlononeil.com>
2017-10-13 21:14:48 -0400

    Changed search to pull values from database instead of local 'API'

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,21 @@
 class CoursesController < ApplicationController
 
   def index
-    if params[:course]
-      @courses = Course.search(params[:course])
+    @courses = Course.all
+
+    @courses = if params[:course]
+      course_filter_search(params[:course])
+    else
+      @course = []
     end
+
   end
 
+
+  private
+
+  def course_filter_search(query)
+    query = '%' + query + '%'
+    Course.where('name ILIKE ?', query )
+  end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,21 +2,4 @@ class Course < ApplicationRecord
   has_many :meeting_sections
   has_and_belongs_to_many :users
 
-  def self.search(course)
-    courses = []
-    response = HTTParty.get("http://localhost:4242/1.0/courses/filter?q=name:'#{course} OR +code:'#{course}&limit=100")
-    parsed_reponse = JSON.parse(response.body)
-    parsed_reponse.each do |course|
-      courses << {
-        name: course['name'],
-        code: course['code'],
-        description: course['description']
-      }
-    end
-    return courses
-  end
-
-
-
-
 end
