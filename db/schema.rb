0c7011bce11428634ecc1262c546dd8298a239ff
Marlon O'Neil <hello@marlononeil.com>
2017-10-18 12:52:58 -0400

    Set up testing platforms on 'testing branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013202002) do
+ActiveRecord::Schema.define(version: 20171017231854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20171013202002) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.string "access_token"
+    t.string "username"
   end
 
 end
